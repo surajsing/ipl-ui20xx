6dca87148aa71fb5a6057b98dd5cd860d97d1543
surajsing <surajsingh02468@gmail.com>
2019-04-20 17:11:54 +0530

    updated with break line in contestant data and value

--- src/app/sattebaaz-last-update/sattebaaz-last-update.component.ts
+++ src/app/sattebaaz-last-update/sattebaaz-last-update.component.ts
@@ -1,3 +1,4 @@
+import { MatchFixturesData } from './../sattebaaz-stats-board/match-fixtures-data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SattebaazLastUpdateComponent implements OnInit {
 
-  constructor() { }
+  lastUpdatedDate: String;
+
+  constructor(private matchDate: MatchFixturesData) { }
+
+  getLastUpdatedDate(matchData: any) {
+    let lastDateIteration: Date;
+    matchData.forEach(element => {
+      if (element['winner'] != 'NYD') {
+        lastDateIteration = element['date'];
+      }
+      return;
+    });
+    this.setLastUpdatedDate(lastDateIteration);
+  }
+
+  setLastUpdatedDate(lastUpdate: Date) {
+    let dateArr = lastUpdate.toDateString().split(' ');
+    this.lastUpdatedDate = dateArr[2] + '-' + dateArr[1].toUpperCase() + '-' + dateArr[3];
+  }
 
   ngOnInit() {
+    this.getLastUpdatedDate(this.matchDate.getMatchData());
   }
 
 }
