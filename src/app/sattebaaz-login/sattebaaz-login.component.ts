32d4d18c557b649d2f327a8f01a48c9642cbcfff
surajsing <surajsingh02468@gmail.com>
2019-04-21 21:06:19 +0530

    bug fixed

--- src/app/sattebaaz-login/sattebaaz-login.component.ts
+++ src/app/sattebaaz-login/sattebaaz-login.component.ts
@@ -22,8 +22,8 @@ export class SattebaazLoginComponent implements OnInit {
     return this.form.controls;
   }
 
-  setUsername() {
-    localStorage.setItem('username', this.fields.username.value);
+  setUsername(loginUser : string) {
+    localStorage.setItem('username', loginUser);
     this.router.navigate(['/sattebaaz-home']);
   }
 
@@ -40,65 +40,68 @@ export class SattebaazLoginComponent implements OnInit {
   onSubmit() {
     if (!this.fields.username.errors && this.fields.username.value) {
       localStorage.removeItem('username');
-      switch (this.fields.username.value) {
+      let userName : string = this.fields.username.value;
+      let setName = userName.toLowerCase();
+      console.log(setName);
+      switch (setName) {
         case 'suraj': if (this.fields.password.value === 'singh') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'kalpesh': if (this.fields.password.value === 'kalpesh') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'kailash': if (this.fields.password.value === 'kailash') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'neeraj': if (this.fields.password.value === 'neeraj') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'pooja': if (this.fields.password.value === 'pooja') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'snehal': if (this.fields.password.value === 'snehal') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'manasi': if (this.fields.password.value === 'manasi') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'radhika': if (this.fields.password.value === 'radhika') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'shruti': if (this.fields.password.value === 'shruti') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'param': if (this.fields.password.value === 'param') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'pratik': if (this.fields.password.value === 'pratik') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'tushar': if (this.fields.password.value === 'tushar') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'neha': if (this.fields.password.value === 'neha') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'siddhu': if (this.fields.password.value === 'siddhu') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         case 'poonam': if (this.fields.password.value === 'poonam') {
-          this.setUsername();
+          this.setUsername(setName);
         } else this.resetForm();
           break
         default: this.resetForm();
